9d78e06be92e71cd60378d7b362de07f0decf273
David Vémola <Vemola.David@stud.vassboskovice.cz>
2024-05-14 09:24:26 +0200

    Záporná čísla

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -9,18 +9,29 @@ Module Program
         Console.WriteLine("Zadej èíslo: ")
         vstup = Console.ReadLine
 
-        If vstup < 10 Then
-            If Integer.TryParse(vstup, cislo) Then
-                For i = 1 To cislo
-                    soucet = soucet + i
-                Next
-                Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+
+        If vstup > 0 Then
+            If vstup < 10 Then
+                If Integer.TryParse(vstup, cislo) Then
+                    For i = 1 To cislo
+                        soucet = soucet + i
+                    Next
+                    Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+                Else
+                    Console.WriteLine("Chyba musíte zadat jen celá èísla")
+                End If
             Else
-                Console.WriteLine("Chyba musíte zadat jen celá èísla")
+                Console.WriteLine("Zadali jste moc velkou hodnotu.")
             End If
         Else
-            Console.WriteLine("Zadali jste moc velkou hodnotu.")
+            Console.WriteLine("Nelze poèítat se zápornýmý èísly")
         End If
+
+
+
+
+
+
 
 
 
