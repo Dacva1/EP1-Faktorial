2fd38809bfaf5cbafc23763e0cc808610e49b9a4
David Vémola <Vemola.David@stud.vassboskovice.cz>
2024-05-14 08:55:50 +0200

    If else

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -2,17 +2,23 @@ Imports System
 
 Module Program
   Sub Main(args As String())
-        Dim cislo As Integer
+        Dim vstup As String
         Dim soucet As Integer = 0
+        Dim cislo As Integer
 
         Console.WriteLine("Zadej èíslo: ")
-        cislo = Console.ReadLine
+        vstup = Console.ReadLine
 
-        For i = 1 To cislo
-            soucet = soucet + i
-        Next
 
-        Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+        If Integer.TryParse(vstup, cislo) Then
+            For i = 1 To cislo
+                soucet = soucet + i
+            Next
+            Console.WriteLine($"Souèet od 1 do {cislo} je {soucet}")
+        Else
+            Console.WriteLine("Chyba musíte zadat jen celé èísla")
+        End If
+
 
     End Sub
 End Module
